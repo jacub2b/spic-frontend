01ef4f5c068242a56e7861031365a5e9ee21cc4f
nbav12 <nbav12@gmail.com>
2023-03-20 20:30:40 +0200

    Init structure

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HelloWorld from "@/components/HelloWorld";
+
+const routes = [
+    {
+        path: '/',
+        component: HelloWorld
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
