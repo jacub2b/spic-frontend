405b3bccc746e113e8b79dcd80adc52082557cd6
nbav12 <nbav12@gmail.com>
2023-03-26 21:47:12 +0300

    Add welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <v-app>
     <Navbar/>
+    <Welcome/>
   </v-app>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
+import Welcome from "@/components/Welcome";
 
 export default {
   name: 'App',
   components: {
+    Welcome,
     Navbar
   }
 }
---
+++ src/components/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="require('../assets/logo.png')" alt="spic_logo"/>
+  <h1 class="text-h3 text-center mt-5">Welcome to SPIC!</h1>
+  <p class="text-h5 text-center mt-5">The application that lets you to speak with pictures.</p>
+  <p class="text-h5 text-center mt-5">To start, choose your category (from the left menu) and browse <br>
+                                      through the pictures to tell what you want by them.</p>
+</template>
+
+<script>
+export default {
+  name: "Welcome"
+}
+</script>
+
+<style scoped>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
+  width: 20%;
+}
+
+p {
+  font-family: "Franklin Gothic Book", sans-serif !important;
+
+}
+</style>
